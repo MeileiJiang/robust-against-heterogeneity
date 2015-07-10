15327c71b90d88a467545bee07037516a5c7e6bb
Meilei Jiang <jiangm@live.unc.edu>
2015-07-10 17:00:56 -0400

    SVA

--- Surrogate Variable Analysis/R/svaTrial.R
+++ Surrogate Variable Analysis/R/svaTrial.R
@@ -12,3 +12,37 @@ library(sva)
 
 load("rdata/toyexample1.RData")
 
+M <- train %>% select(-y, -label)
+rownames(M) =  c("X1", "X2")
+
+Y <- train %>% select(y)
+
+glm.sol = glm(y ~ .,family = 'binomial', data = train)
+summary(glm.sol)
+# make model matrix -------------------------------------------------------
+## full model 
+mod = model.matrix(~ y, data = train)
+## null model
+mod0 = model.matrix(~ 1, data = train)
+
+
+# estimate the number of latent factors that need to be estimated ---------
+n.sv = num.sv(t(M),mod,method="be")
+
+
+
+# estimate the surrogate variables
+svobj = sva(t(M),mod,mod0,n.sv=1)
+sv = svobj$sv
+pprob.gam = svobj$pprob.gam
+
+
+# adjusting for surrogate variables
+pValues = f.pvalue(t(M),mod,mod0)
+qValues = p.adjust(pValues,method="BH")
+
+modSv = cbind(mod,svobj$sv)
+mod0Sv = cbind(mod0,svobj$sv)
+pValuesSv = f.pvalue(t(M),modSv,mod0Sv)
+qValuesSv = p.adjust(pValuesSv,method="BH")
+
--- Surrogate Variable Analysis/R/toyexample.R
+++ Surrogate Variable Analysis/R/toyexample.R
@@ -6,7 +6,7 @@
 ## Author: Meilei
 ###########################################################################################################
 
-# Class1: X ~ pi_1 N([-4, 4], I_2 ) + (1 - pi_1) N([4, 4], I_2)
+# Class1: X ~ pi_1 N([-4, 4, rep(0, 198)], I_200 ) + (1 - pi_1) N([4, 4, rep(0, 198)], I_200)
 # Class2: X ~ pi_2 N([-4, -4], I_2 ) + (1 - pi_2) N([4, -4], I_2)
 
 library(dplyr)
@@ -16,7 +16,7 @@ library(mnormt)
 # function to generate Gaussion mixture -----------------------------------
 
 toy = function(n, pi, mu1, mu2, cov1, cov2){
-  x = matrix(rep(NA, 2 * n), ncol = 2)
+  x = matrix(rep(NA, 200 * n), ncol = 200)
   k = rbinom(n, 1, pi)
   for(i in 1:n){
     x[i, ] = (1 - k[i]) * rmnorm(mean = mu1, varcov = cov1) + k[i] * rmnorm(mean = mu2, varcov = cov2)
@@ -28,27 +28,30 @@ toy = function(n, pi, mu1, mu2, cov1, cov2){
 # generate the data -------------------------------------------------------
 
 
-n = 200
+n = 50
 pi = 0.5
-mu1 = c(-4, 4)
-mu2 = c(4, 4)
-mu3 = c(-4, -4)
-mu4 = c(4, -4)
-cov1 = diag(c(1,1))
-cov2 = diag(c(1,1))
-cov3 = diag(c(1,1))
-cov4 = diag(c(1,1))
+mu1 = c(-4, 4, rep(0, 98))
+mu2 = c(4, 4, rep(0, 98))
+mu3 = c(-4, -4, rep(0, 98))
+mu4 = c(4, -4, rep(0, 98))
+cov1 = diag(rep(1,100))
+cov2 = diag(rep(1,100))
+cov3 = diag(rep(1,100))
+cov4 = diag(rep(1,100))
 
 M1 = toy(n, pi, mu1, mu2, cov1, cov2)
 M2 = toy(n, pi, mu3, mu4, cov3, cov4)
 M = rbind(M1$data, M2$data)
 label = c(M1$label, M2$label)
-train = data.frame(y = c(rep(1,200), rep(-1, 200)), M, label = label)
+train = data.frame(y = c(rep("A",50), rep("B", 50)), M, label = as.factor(label))
 
 
 # visualize data ----------------------------------------------------------
 
-ggplot(data = train, aes(x = X1, y = X2, color = factor(y))) + geom_point(aes(shape = factor(label)))
+ggplot(data = train, aes(x = X1, y = X2, color = y)) + geom_point(aes(shape = label))
+ggplot(data = train, aes(x = X1, y = X3, color = y)) + geom_point(aes(shape = label))
+ggplot(data = train, aes(x = X2, y = X3, color = y)) + geom_point(aes(shape = label))
+ggplot(data = train, aes(x = X3, y = X4, color = y)) + geom_point(aes(shape = label))
 
 
 # save the simulation data ------------------------------------------------
